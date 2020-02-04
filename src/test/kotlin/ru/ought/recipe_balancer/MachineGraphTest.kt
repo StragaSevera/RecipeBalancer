bd36f425e59d6f3a5856c6b5490e192eda31f593
Straga Severa <ought@yandex.ru>
2020-02-04 19:35:09 +0300

    Refactor graph tests, start again

--- src/test/kotlin/ru/ought/recipe_balancer/MachineGraphTest.kt
+++ src/test/kotlin/ru/ought/recipe_balancer/MachineGraphTest.kt
@@ -1,6 +1,7 @@
 package ru.ought.recipe_balancer
 
 import ch.tutteli.atrium.api.fluent.en_GB.toBe
+import ch.tutteli.atrium.api.fluent.en_GB.toBeWithErrorTolerance
 import ch.tutteli.atrium.api.verbs.expect
 import org.spekframework.spek2.Spek
 
@@ -8,86 +9,55 @@ import org.spekframework.spek2.Spek
 @Suppress("LocalVariableName")
 object MachineGraphTest : Spek({
     group("basic tests") {
-        val Na by memoized { Ingredient("Na") }
-        val H2O by memoized { Ingredient("H2O") }
-        val NaOH by memoized { Ingredient("NaOH") }
-        val NaOHPile by memoized { Ingredient("Tiny Pile of NaOH") }
-        val H2 by memoized { Ingredient("H2") }
-        val pumpkin by memoized { Ingredient("Pumpkin") }
-        val pumpkinSeed by memoized { Ingredient("Pumpkin Seed") }
-        val seedOil by memoized { Ingredient("Seed Oil") }
-        val CO by memoized { Ingredient("CO") }
-        val methanol by memoized { Ingredient("Methanol") }
-        val glycerol by memoized { Ingredient("Glycerol") }
-        val bioDiesel by memoized { Ingredient("Bio Diesel") }
+        val a by memoized { Ingredient("A") }
+        val b by memoized { Ingredient("B") }
+        val c by memoized { Ingredient("C") }
+        val d by memoized { Ingredient("D") }
+        val e by memoized { Ingredient("E") }
+        val f by memoized { Ingredient("F") }
 
-        val machineNaOH by memoized {
-            MachineStack(
-                Recipe(
-                    listOf(Stack(Na, 1), Stack(H2O, 3000)),
-                    listOf(Stack(NaOH, 3), Stack(H2, 1000)),
-                    30f, 30f, "Chemical Reactor"
-                ), 1
-            )
-        }
-        val machineNaOHPile by memoized {
-            MachineStack(
-                Recipe(
-                    listOf(Stack(NaOH, 1)),
-                    listOf(Stack(NaOHPile, 9)),
-                    4f, 5f, "Packager"
-                ), 1
-            )
-        }
-        val machinePumpkinSeed by memoized {
-            MachineStack(
-                Recipe(
-                    listOf(Stack(pumpkin, 1)),
-                    listOf(Stack(pumpkinSeed, 4)),
-                    32f, 0.8f, "Packager"
-                ), 1
-            )
-        }
-        val machineSeedOil by memoized {
-            MachineStack(
-                Recipe(
-                    listOf(Stack(pumpkinSeed, 1)),
-                    listOf(Stack(seedOil, 10)),
-                    2f, 1.6f, "Fluid Extractor"
-                ), 1
-            )
-        }
-        val machineMethanol by memoized {
-            MachineStack(
-                Recipe(
-                    listOf(Stack(H2, 1000), Stack(CO, 500)),
-                    listOf(Stack(methanol, 1500)),
-                    120f, 7.5f, "Chemical Reactor"
-                ), 1
-            )
-        }
-        val machineBioDiesel by memoized {
-            MachineStack(
-                Recipe(
-                    listOf(Stack(NaOHPile, 1), Stack(seedOil, 6000), Stack(methanol, 1000)),
-                    listOf(Stack(glycerol, 1000), Stack(bioDiesel, 6000)),
-                    30f, 30f, "Chemical Reactor"
-                ), 1
+        val recipeABC by memoized {
+            Recipe(
+                listOf(Stack(a, 1)),
+                listOf(Stack(b, 3), Stack(c, 2)),
+                30f, 1f
             )
         }
 
+        val recipeCDEF by memoized {
+            Recipe(
+                listOf(Stack(c, 4), Stack(d, 1)),
+                listOf(Stack(e, 2), Stack(f, 3000)),
+                30f, 1f
+            )
+        }
 
         test("can be linked") {
+            val machineABC = MachineStack(recipeABC)
+            val machineCDEF = MachineStack(recipeCDEF)
             val sut = MachineGraph()
 
-            sut.link(machinePumpkinSeed, machineSeedOil, pumpkinSeed)
+            sut.link(machineABC, machineCDEF, c)
 
             expect(sut.links).toBe(
                 mapOf(
-                    machinePumpkinSeed to
-                            listOf(MachineLink(machinePumpkinSeed, machineSeedOil, pumpkinSeed))
+                    machineABC to listOf(
+                        MachineLink(machineABC, machineCDEF, c)
+                    )
                 )
             )
+        }
+
+        test("can balance forward graph with two nodes") {
+            val machineABC = MachineStack(recipeABC)
+            val machineCDEF = MachineStack(recipeCDEF)
+            val sut = MachineGraph()
+            sut.link(machineABC, machineCDEF, c)
+
+            sut.balanceForward()
+
+            expect(machineABC.boundedRatio).toBeWithErrorTolerance(1f, 0.01f)
+            expect(machineCDEF.boundedRatio).toBeWithErrorTolerance(0.5f, 0.01f)
         }
     }
 })
